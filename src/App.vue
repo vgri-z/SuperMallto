8645f7f84553a68e3c9a7acfd5412b206db212b5
vgri <vgri199712@163.com>
2023-11-28 16:40:48 +0800

    bug修改

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,15 @@
 </template>
 
 <script>
-import MainTabBar from "components/content/mainTabbar/MinTabBar";
-  export default {
-    name: "app",
-    components: {
-      MainTabBar
-    }
-  };
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
+export default {
+  name: "app",
+  components: {
+    MainTabBar
+  }
+};
 </script>
 
 <style>
-  @import "./assets/css/base.css";
+@import "./assets/css/base.css";
 </style>
